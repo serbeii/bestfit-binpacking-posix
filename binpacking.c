0ef7cc80f415a418c4d2e5b5e6aa8084759aa353
Cem Serimer <cem.serimer@tedu.edu.tr>
2025-03-15 01:28:27 +0300

    BestFit binpacking single threaded implementation.

--- binpacking.c
+++ binpacking.c
@@ -1,24 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define BIN_SIZE 10
-#define MAX_BINS 100
+#define BIN_CAPACITY 10
+#define ITEM_COUNT 500
 
-typedef struct {
-    int size;
-    int bin_index;
-} Item;
+static int total_bins = 0;
 
-typedef struct {
-    int data[BIN_SIZE];
-    int current_load;
-    Item items[10];
-} Bin;
+void bestFit(int start_index, int size, int capacity);
+void createData(int size, int capacity);
 
-struct Node {
-    Bin bin;
-    struct Node* next;
-}typedef BinList;
+int main() {
+    createData(ITEM_COUNT, BIN_CAPACITY);
+    bestFit(0, ITEM_COUNT, BIN_CAPACITY);
+    printf("Number of total_bins required in Best Fit: %d\n", total_bins);
+}
 
-int insert_to_list(BinList **list, Bin bin);
+void createData(int size, int capacity) {
+    FILE* file;
+    file = fopen("data.txt", "w+");
+    srand(time(NULL));
+    for (int i = 0; i < size; i++) {
+        fprintf(file, "%d\n", rand() % capacity + 1);
+    }
+    fclose(file);
+}
 
-int main() {}
+void bestFit(int start_index, int size, int capacity) {
+    int bin_capacity[size];
+    int used_bins = 0;
+    FILE* file;
+    file = fopen("data.txt", "r");
+    int current_line = 0;
+    // skip to start index
+    while (current_line < start_index &&
+           fscanf(file, "%d", (int*)NULL) != EOF) {
+        current_line++;
+    }
+    for (int i = 0; i < size; i++) {
+        // read current data from the file
+        int current_data;
+        fscanf(file, "%d", &current_data);
+
+        int min = capacity + 1;
+        int bin_index = 0;
+
+        // Find the best bin
+        for (int j = 0; j < used_bins; j++) {
+            if (bin_capacity[j] >= current_data &&
+                bin_capacity[j] - current_data < min) {
+                bin_index = j;
+                min = bin_capacity[j] - current_data;
+            }
+        }
+
+        // No best bin exists
+        if (min == capacity + 1) {
+            bin_capacity[used_bins] = capacity - current_data;
+            used_bins++;
+        } else  // Best bin is found, reduce remaining capacity
+        {
+            bin_capacity[bin_index] -= current_data;
+        }
+    }
+    total_bins += used_bins;
+}
--- heuristic.md
+++
@@ -1,7 +0,0 @@
-* Create the array of bins
-* Create the items to be packed in the bins
-* Loop:
-    * From the list, find the most full bin the current item can fit into and fit accordingly, if the bin gets full remove it from the list.
-    * If there is no such bin, insert a new bin to the list, insert the item to the new bin
-    * If there is no data exit
-* Print the time it takes and how many bins it took.
